c762ac8c02f4301c0efa7009d41f05b060458e05
LADE Robotics <fakeemail@fakemailservice.com>
2025-10-23 13:14:07 -0600

    Adding my personal TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Elliotts_TeleOp.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Elliotts_TeleOp extends LinearOpMode {
+    private void reallyNiceCode() {
+        boolean hi;
+    }
+
+    @Override
+    public void runOpMode() {
+        //initialization code
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+        }
+    }
+}
