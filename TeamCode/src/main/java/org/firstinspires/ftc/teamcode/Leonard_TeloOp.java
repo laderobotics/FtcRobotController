9a59053678a29458749294621fd9b298471f45e2
leonardonavarro <hackcity21797@gmail.com>
2025-10-23 13:22:19 -0600

    leonard opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Leonard_TeloOp.java
@@ -0,0 +1,23 @@
+//TODO: This is my note.
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Leonard TeleOp", group = "robot")
+public class Leonard_TeloOp extends LinearOpMode {
+    private void reallyNiceCode() {
+        boolean hi;
+    }
+
+    @Override
+    public void runOpMode() {
+        //initialization code
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+        }
+    }
+}
