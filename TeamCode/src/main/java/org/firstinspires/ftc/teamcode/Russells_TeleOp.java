d4d1adde3d2a276a2fade5c043d46c660e6491e5
laderobotics <hackcity21797@gmail.com>
2025-10-23 14:08:15 -0600

    adding my own opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Russells_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Russells_TeleOp.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name = "Russells TeleOp", group = "robot")
 public class Russells_TeleOp extends LinearOpMode {
-    private void reallyNiceCode() {
+    private void boloneysandwhich() {
         boolean hi;
     }
 
@@ -18,5 +18,4 @@ public class Russells_TeleOp extends LinearOpMode {
 
         }
     }
-}
 }
