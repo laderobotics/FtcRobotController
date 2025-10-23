da60d0cc656f341dcddfe0ab6942f16d9b7d7d23
LADE Robotics <fakeemail@fakemailservice.com>
2025-10-23 14:19:46 -0600

    finished demo code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Russells_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Russells_TeleOp.java
@@ -19,4 +19,3 @@ public class Russells_TeleOp extends LinearOpMode {
         }
     }
 }
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
@@ -4,21 +4,48 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Main TeleOp", group = "robot")
 public class TeleOp_Test extends LinearOpMode {
+    int myVariable = 2;
+    double myDouble = 4.221312;
+    boolean myBoolean = true;
+    Servo myServo;
+    DcMotor myMotor;
+
     private void reallyNiceCode() {
-        boolean hi;
+        if(gamepad1.a) {
+            myVariable = 3;
+            myServo.setPosition(0.8);
+        }
+        if(gamepad1.b) {
+            myVariable = -2;
+            myServo.setPosition(0.2);
+        }
+    }
+
+    private void telemetry() {
+        telemetry.addData("myVariable is equal to ", myVariable);
+        telemetry.addData("myServo is equal to", myServo.getPosition());
+        telemetry.update();
+    }
+
+    private void initializeHardware() {
+        myServo = hardwareMap.get(Servo.class, "servo");
     }
 
     @Override
     public void runOpMode() {
         //initialization code
+        initializeHardware();
 
         waitForStart();
 
         while(opModeIsActive()) {
-
+            reallyNiceCode();
+            telemetry();
         }
     }
 }
