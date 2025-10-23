d9c22048d70880f66e76a6fe28f8e516b3f0216e
LADE Robotics <fakeemail@fakemailservice.com>
2025-10-23 12:48:42 -0600

    Adding my code to the repository

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
@@ -1,2 +1,20 @@
 //TODO: This is my note.
 package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Main TeleOp", group = "robot")
+public class TeleOp_Test extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        //initialization code
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+        }
+    }
+}
