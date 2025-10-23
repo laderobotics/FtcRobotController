928d2f07f8cab8f8eda41b60d136449bfc63dd79
joledsotomercado <hackcity21797@gmail.com>
2025-10-23 13:22:42 -0600

    joledsteleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/joledteleOp.java
@@ -0,0 +1,23 @@
+//TODO: This is my note.
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "joled TeleOp", group = "robot")
+public class joledteleOp extends LinearOpMode {
+    private void reallyNiceCode() {
+        boolean hi;
+    }
+
+    @Override
+    public void runOpMode() {
+        //initialization code
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+        }
+    }
+}
