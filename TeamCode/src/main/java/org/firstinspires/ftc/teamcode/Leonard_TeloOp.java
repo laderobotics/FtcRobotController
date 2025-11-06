0ff247bb380fdbf2a30acb569d1c09a0f01d9468
leonardonavarro <hackcity21797@gmail.com>
2025-11-06 14:09:35 -0700

    leonard opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Leonard_TeloOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Leonard_TeloOp.java
@@ -3,11 +3,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "Leonard TeleOp", group = "robot")
 public class Leonard_TeloOp extends LinearOpMode {
-    private void reallyNiceCode() {
-        boolean hi;
+
+    int myVarible = 2;
+    private void launchtest1() {
+        DcMotor LaunchMotor = hardwareMap.dcMotor.get("LaunchMotor");
+        if (gamepad1.a) {
+            LaunchMotor.setPower(1.0)
+        }else{
+            LaunchMotor.setPower(0.0);
+        }
     }
 
     @Override
@@ -17,7 +25,7 @@ public class Leonard_TeloOp extends LinearOpMode {
         waitForStart();
 
         while(opModeIsActive()) {
-
+            launchtest1();
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readme.md
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/readme.md
@@ -1,4 +1,4 @@
-## TeamCode Module
+BH## TeamCode Module
 
 Welcome!
 
