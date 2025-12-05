b0ceb6e0f1b6aa40ace7043d05265ffe02e46159
leonardonavarro <hackcity21797@gmail.com>
2025-12-05 15:15:38 -0700

    leonard opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Leonard_TeloOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Leonard_TeloOp.java
@@ -3,18 +3,36 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "Leonard TeleOp", group = "robot")
 public class Leonard_TeloOp extends LinearOpMode {
 
     int myVarible = 2;
+    private CRServo turningtableservo;
     private void launchtest1() {
         DcMotor LaunchMotor = hardwareMap.dcMotor.get("LaunchMotor");
         if (gamepad1.a) {
-            LaunchMotor.setPower(0.1);
-        }else{
+            LaunchMotor.setPower(0.5);
+        } else if (gamepad1.b) {
+            LaunchMotor.setPower(0.4);
+        } else if (gamepad1.y) {
+            LaunchMotor.setPower(0.3);
+        } else if (gamepad1.x) {
+            LaunchMotor.setPower(0.2);
+        } else{
             LaunchMotor.setPower(0.0);
+        }
+    }
+    private void turnTable() {
+        turningtableservo = hardwareMap.get(CRServo.class, "turningtableservo");
+        if (gamepad1.left_bumper) { // Example: Press 'A' button to turn clockwise
+            turningtableservo.setPower(-1.0); // Adjust speed as needed
+        } else if (gamepad1.right_bumper) { // Example: Press 'B' button to turn counter-clockwise
+            turningtableservo.setPower(1.0); // Adjust speed as needed
+        } else {
+            turningtableservo.setPower(0.0); // Stop when no button is pressed
         }
     }
 
@@ -33,6 +51,7 @@ public class Leonard_TeloOp extends LinearOpMode {
 
         while(opModeIsActive()) {
             launchtest1();
+            turnTable();
         }
     }
 }
