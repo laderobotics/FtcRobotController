8fc8a74b45ad1d58e08a704e824fed00e7029361
laderobotics <hackcity21797@gmail.com>
2025-12-09 13:12:34 -0700

    adding my own opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/Intake.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake {
+
+    private DcMotor intakeMotor;
+
+    public void init(HardwareMap hwMap){
+        intakeMotor = hwMap.get(DcMotor.class,"intake_motor");
+        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
@@ -17,7 +17,7 @@ public class TestBench {
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
         // DC Motor
         motor = hwMap.get(DcMotor.class,"intake_motor");
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motor.setDirection(DcMotorSimple.Direction.REVERSE);
         ticksPerRev = motor.getMotorType().getTicksPerRev();
