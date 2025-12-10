122b0d2077d5616b8d8371cbe09a0a0a0baea8e2
Mr. Loyd <mrloyd@ladecharter.org>
2025-12-09 22:26:29 -0700

    Mr. Loyd practicing java following tutorials at https://www.youtube.com/watch?v=gXegsVvXLd0&list=PLRHdgFNRLyaPiZ5rvINwMmGMHEIL9usla&index=6.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorExample.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
 import org.openftc.easyopencv.OpenCvCameraBase;
-
+@Disabled
 @TeleOp
 public class ColorSensorExample extends OpMode {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceTest.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
-
+@Disabled
 @TeleOp
 public class DistanceTest extends OpMode {
 
