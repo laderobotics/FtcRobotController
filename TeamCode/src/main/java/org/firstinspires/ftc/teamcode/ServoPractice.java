c717e3f9b529128152b9474cfa816cb0345706cf
laderobotics <hackcity21797@gmail.com>
2025-12-15 13:13:55 -0700

    Starting robot Teleop OpMode with Leonard and Mr. Loyd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoPractice.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
+import org.firstinspires.ftc.teamcode.tutorials.TestBench;
 @Disabled
 @TeleOp
 public class ServoPractice extends OpMode {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/TestBench.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/TestBench.java
@@ -1,6 +1,5 @@
-package org.firstinspires.ftc.teamcode.mechanisms;
+package org.firstinspires.ftc.teamcode.tutorials;
 
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
