01371ce56a59efc4a632809cf27e7e1a1d211c21
joledsotomercado <hackcity21797@gmail.com>
2025-12-17 14:23:32 -0700

    joledsteleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+
+@TeleOp
+public class HelloWorld extends OpMode {
+    @Override
+    public void init() {
+
+        telemetry.addData("hello","world"
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/joledteleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/joledteleOp.java
@@ -5,19 +5,4 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "joled TeleOp", group = "robot")
-public class joledteleOp extends LinearOpMode {
-    private void reallyNiceCode() {
-        boolean hi;
-    }
-
-    @Override
-    public void runOpMode() {
-        //initialization code
-
-        waitForStart();
-
-        while(opModeIsActive()) {
-
-        }
-    }
-}
+public class joledteleOp extends LinearOpM
