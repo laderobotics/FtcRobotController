79d1c2314fe7ab352a681724b7b4ffb413aba286
leonardonavarro <hackcity21797@gmail.com>
2026-02-18 13:53:50 -0700

    Adding more complicated autonomous, not done yet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelSpeedTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FlywheelSpeedTuner.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -8,7 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import org.firstinspires.ftc.teamcode.mechanisms.Launcher;
-
+@Disabled
 @TeleOp
 public class FlywheelSpeedTuner extends OpMode {
     public DcMotorEx flywheelMotor;
