46f15276c2a4b1f04a182d2fc5b74da40db1210d
audrinaromero <Hackcity21797@gmail.com>
2026-08-20 13:30:15 -0600

    Learning java 2026

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/AudrinaHelloWorld.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.tutorials;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class AudrinaHelloWorld extends OpMode {
+
+
+    @Override
+    public void init_loop()
+
+    @Override
+    public void loop() {
+
+    }
+}
