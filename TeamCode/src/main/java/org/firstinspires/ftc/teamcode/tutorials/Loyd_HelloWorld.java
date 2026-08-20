4b293e5f271899074e035f03bd4ab839068d810b
Mr. Loyd <hackcity21797@gmail.com>
2026-08-20 13:28:57 -0600

    Learning Java 2026

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/Loyd_HelloWorld.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.tutorials;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Loyd_HelloWorld extends OpMode {
+
+
+    @Override
+    public void init() {
+        telemetry.addData("I only wanted to be a math teacher because...","\n")
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
