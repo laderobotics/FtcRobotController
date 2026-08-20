e8677cdab8c4c6dd1f16c7e1c5f4f8cdba308dd8
litzygonzalez <hackcity21797@gmail.com>
2026-08-20 13:29:31 -0600

    learing java 2026

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/litzyhelloworld.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.tutorials;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class litzyhelloworld extends OpMode {
+
+
+    @Override
+    public void init() {
+        telemetry.addData("why do birds fly south in winter","\n")
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
+
