ffb181bff4625fc2d8d7f611fefd359ff7833f92
litzygonzalez <hackcity21797@gmail.com>
2026-08-24 14:38:32 -0600

    learing java 2026

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/litzybuttontest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.tutorials;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class litzybuttontest extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+      if (gamepad1.a){
+          telemetry.addData("hehheheheh",gamepad1.a);
+      }
+      if (gamepad1.dpad_up){
+          telemetry.addData("Hi am a tree.","up");
+      }
+      if (gamepad1.back){
+    }
+}
