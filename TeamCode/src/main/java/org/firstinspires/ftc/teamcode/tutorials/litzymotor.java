2ffb9670c219567c42d6bc26dafa87ad9999b95b
litzygonzalez <hackcity21797@gmail.com>
2026-09-09 14:38:41 -0600

    learing java 2026

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorials/litzymotor.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.tutorials;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+public class litzymotor extends OpMode {
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+        double vertica =
+
+    }
+}
